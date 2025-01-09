a7133a3e8f95a6acf7c0ef33f26613e8f069391c
Elorm Daniel <elormcashes@gmail.com>
2025-01-09 19:30:42 +0000

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,12 @@
+# Countdown program using a while loop
+
+# Initialize the starting number
+count = 10
+
+# Countdown loop
+while count > 0:
+    print(count)
+    count -= 1  # Decrease the count by 1
+
+# Print a final message after the countdown
+print("Liftoff!")
